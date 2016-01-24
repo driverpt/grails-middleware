b1da1e540f451f42ee48e92a8cab5f494e7b46b0
Luís Duarte <lduarte@luisduarte.net>
2016-01-24 12:08:36 +0000

    Renamed Test to match new artefact name

--- integration-test-app/src/integration-test/groovy/grails/middleware/MiddlewareConfigSpec.groovy
+++ integration-test-app/src/integration-test/groovy/grails/middleware/MiddlewareConfigSpec.groovy
@@ -1,26 +1,26 @@
 package grails.middleware
 import grails.artefact.middleware.Middleware
 import grails.core.GrailsApplication
-import grails.plugins.GrailsPluginManager
 import grails.test.mixin.integration.Integration
 import grails.transaction.Rollback
 import spock.lang.Specification
 
 @Integration
 @Rollback
-class GrailsMiddlewareConfigSpec extends Specification {
+class MiddlewareConfigSpec extends Specification {
 
     def GrailsApplication grailsApplication
 
     def setup() {
+        // Check application.yml for config
     }
 
     def cleanup() {
     }
 
     void "Test Middleware Configuration"() {
-        given: "GrailsMiddlewareFilter"
-        def grailsMiddlewareFilter = grailsApplication.mainContext.getBean("grailsMiddlewareFilter").filter
+        given: "MiddlewareFilter"
+        def grailsMiddlewareFilter = grailsApplication.mainContext.grailsMiddlewareFilter.filter
 
         expect: "Chain size to be 2"
         grailsMiddlewareFilter.chain.size == 2
